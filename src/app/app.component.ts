7df02965a799e6ada22a3884e7dbe8049dd6dea6
Learn <happy.anicut@gmail.com>
2017-12-05 04:59:12 +0530

    Created sample components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,46 @@
 import { Component } from '@angular/core';
+import {Counter}   from "./counter/counter";
+import {Clock}   from "./clock/clock";
+import  {Person}  from "./Person/Person";
+import  {Multiplication}  from "./Multiplication/Multiplication";
 
+
+ 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+      <div class="row" >
+         <div class="col-md-2" >
+             Counter 
+         </div>
+         <div class="col-md-4" >
+             <counter></counter> 
+         </div>
+      </div>
+      <div class="row" >
+         <div class="col-md-2" >
+             Clock 
+         </div>
+         <div class="col-md-10" >
+            <clock></clock>
+         </div>
+      </div>
+      <div class="row" >
+         <div class="col-md-2" >
+             Person 
+         </div>
+         <div class="col-md-8" >
+            <person></person>
+         </div>
+      </div>
+      <div class="row" >
+         <div class="col-md-2" >
+             Multiply 
+         </div>
+         <div class="col-md-8" >
+            <multiply></multiply>
+         </div>
+      </div>
+  `,
 })
 export class AppComponent  { name = 'Jagadeesh'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule }  from "@angular/forms";
 
+import {Counter}   from "./counter/counter";
+import {Clock}   from "./clock/clock";
+import  {Person}  from "./Person/Person";
+import  {Multiplication}  from "./Multiplication/Multiplication";
 import { AppComponent }  from './app.component';
 
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,FormsModule ],
+  declarations: [ AppComponent ,Counter,Clock,Person,Multiplication],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
